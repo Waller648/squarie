9323aa0298befbb0ca1af340c2336036ade5a8c4
Waller648 <77145774+Waller648@users.noreply.github.com>
2025-08-28 18:16:47 +0300

    Create colors.js

---
+++ src/colors.js
@@ -0,0 +1,158 @@
+// restrict to only 31 colors supported
+colors = [
+    {
+        "hex": "#000000",
+        "color": "black",
+        "id": 0
+    },
+    {
+        "hex": "#898D90",
+        "color": "grey",
+        "id": 30
+    },
+    {
+        "hex": "#D4D7D9",
+        "color": "light grey",
+        "id": 1
+    },
+    {
+        "hex": "#FF99AA",
+        "color": "light pink",
+        "id": 2
+    },
+    {
+        "hex": "#FF4500",
+        "color": "red",
+        "id": 23
+    },
+    {
+        "hex": "#FFA800",
+        "color": "orange",
+        "id": 15
+    },
+    {
+        "hex": "#9C6926",
+        "color": "brown",
+        "id": 4
+    },
+    {
+        "hex": "#FFD635",
+        "color": "yellow",
+        "id": 5
+    },
+    {
+        "hex": "#7EED56",
+        "color": "light green",
+        "id": 7
+    },
+    {
+        "hex": "#00CC78",
+        "color": "green",
+        "id": 24
+    },
+    {
+        "hex": "#51E9F4",
+        "color": "light blue",
+        "id": 16
+    },
+    {
+        "hex": "#3690EA",
+        "color": "blue",
+        "id": 9
+    },
+    {
+        "hex": "#2450A4",
+        "color": "dark blue",
+        "id": 8
+    },
+    {
+        "hex": "#B44AC0",
+        "color": "purple",
+        "id": 17
+    },
+    {
+        "hex": "#811E9F",
+        "color": "dark purple",
+        "id": 18
+    },
+    {
+        "hex": "#BE0039",
+        "color": "dark red",
+        "id": 25
+    },
+    {
+        "hex": "#00A368",
+        "color": "dark green",
+        "id": 12
+    },
+    {
+        "hex": "#00756F",
+        "color": "dark teal",
+        "id": 11
+    },
+    {
+        "hex": "#009EAA",
+        "color": "teal",
+        "id": 10
+    },
+    {
+        "hex": "#493AC1",
+        "color": "indigo",
+        "id": 19
+    },
+    {
+        "hex": "#6A5CFF",
+        "color": "periwinkle",
+        "id": 20
+    },
+    {
+        "hex": "#FF3881",
+        "color": "pink",
+        "id": 26
+    },
+    {
+        "hex": "#6D482F",
+        "color": "dark brown",
+        "id": 14
+    },
+    {
+        "hex": "#6D001A",
+        "color": "burgundy",
+        "id": 13
+    },
+    {
+        "hex": "#FFF8B8",
+        "color": "pale yellow",
+        "id": 27
+    },
+    {
+        "hex": "#00CCC0",
+        "color": "light teal",
+        "id": 28
+    },
+    {
+        "hex": "#94B3FF",
+        "color": "lavender",
+        "id": 21
+    },
+    {
+        "hex": "#E4ABFF",
+        "color": "pale purple",
+        "id": 3
+    },
+    {
+        "hex": "#DE107F",
+        "color": "magenta",
+        "id": 22
+    },
+    {
+        "hex": "#FFB470",
+        "color": "beige",
+        "id": 6
+    },
+    {
+        "hex": "#515252",
+        "color": "dark grey",
+        "id": 29
+    }
+]
